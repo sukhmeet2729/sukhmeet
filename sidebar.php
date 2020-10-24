ed3d6527bb2f02dca1933fc33cb50e5c9066b987
Techno Kawal <44253710+kawal1123@users.noreply.github.com>
2020-10-24 18:20:05 +0530

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,14 @@
-
+<div class="list-group list-group-flush p-3" id="sidebar">
+                <div class="list-group-item list-group-item-action px-3 pb-4 bg-transparent text-white mt-2 mb-4">
+                    <div class="d-inline"><i class="fa fa-smile-o fa-lg"></i></div> <div class="pl-4 d-inline-block menu-name"><?php print(ucwords(WEBSITE_NAME)); ?></div>
+                </div>
+                <a href="/index" class="list-group-item list-group-item-action border-0 px-3 bg-cus text-white mb-1 rounded-lg hover-light">
+                    <div class="d-inline"><i class="fa fa-dashboard"></i></div> <div class="pl-4 d-inline-block menu-name">DashBoard</div>
+                </a>
+                <a href="/links" class="list-group-item list-group-item-action border-0 px-3 bg-cus text-white rounded-lg hover-light">
+                    <div class="d-inline"><i class="fa fa-link"></i></div> <div class="pl-4 d-inline-block menu-name">Links</div>
+                </a>
+                <a href="/page" class="list-group-item list-group-item-action border-0 px-3 bg-cus text-white rounded-lg hover-light">
+                    <div class="d-inline"><i class="fa fa-file-text"></i></div> <div class="pl-4 d-inline-block menu-name">Pages</div>
+                </a>
+            </div>
